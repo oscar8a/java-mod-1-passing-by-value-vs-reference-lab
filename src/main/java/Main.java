079922514609c0395af744d8be976bce58b1a900
Oscar 8A <oscarochoacerrato@gmail.com>
2022-06-24 20:53:08 -0400

    tested passing value & reference

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+
+        NumberClass myNumber = new NumberClass();
+        StringClass myString = new StringClass();
+
+        myString.text = "hello... ";
+        String unmodified = "I'm Staying unmodified";
+
+        multiply(myNumber, 2, 3);
+        modifyString(myString, "WRLD!");
+        modifyStringNot(unmodified, " Adding Stuff!");
+
+        System.out.println("Result: " + myNumber.number);
+        System.out.println("modified text: " + myString.text);
+        System.out.println("unmodified text: " + unmodified);
     }
+
+    public static void multiply(NumberClass result, int first, int second){
+        result.number = first * second;
+    }
+
+    public static void modifyString(StringClass result, String input) {
+        result.text = result.text + input;
+    }
+
+    public static void modifyStringNot(String result, String input) {
+        result = result + input;
+    }
+}
+
+class NumberClass {
+    public int number;
+}
+
+class StringClass {
+    public String text;
 }
